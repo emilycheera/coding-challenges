92168990757e70d66fab3afa844ca3e185f49bfc
Emily Cheera <emilycheera@gmail.com>
2020-03-11 15:51:23 -0700

    Reversed list to pop at end.

--- fit_string_to_width.py
+++ fit_string_to_width.py
@@ -16,25 +16,26 @@ def fit_to_width(str, char_limit):
     """Print contents of string without going over limit or breaking words."""
 
     word_list = str.split(" ")
+    word_list.reverse()
     
     while word_list:
 
         if len(word_list) == 1:
-            print(word_list[0])
+            print(word_list[-1])
             break
 
         else:
-            current_line = word_list[0]
+            current_line = word_list[-1]
             current_line_length = len(current_line)
-            word_list.pop(0)
+            word_list.pop()
 
-            while current_line_length + len(word_list[0]) + 1 <= char_limit:
-                current_line += f" {word_list[0]}"
-                word_list.pop(0)
+            while current_line_length + len(word_list[-1]) + 1 <= char_limit:
+                current_line += f" {word_list.pop()}"
                 if len(word_list) == 0:
                  break
             
             print(current_line)
+
 
 
 
