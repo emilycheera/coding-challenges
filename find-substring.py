d720f248baa5ce9a17b63a205807ccc3aac1dddb
Emily Cheera <emilycheera@gmail.com>
2020-03-30 17:23:25 -0700

    Completed find substring challenge.

---
+++ find-substring.py
@@ -0,0 +1,9 @@
+def find_substring(haystack, needle):
+    if needle == "":
+        return 0
+    
+    for i in range(len(haystack) - len(needle) + 1):
+        if haystack[i:i + len(needle)] == needle:
+            return i
+
+    return -1
